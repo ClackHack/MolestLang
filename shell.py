9c67b5b828804d6569534d4f831440dbd4d0fdf8
ClackHack <48455071+ClackHack@users.noreply.github.com>
2021-11-21 15:23:50 -0500

    Add functionality to shell

--- shell.py
+++ shell.py
@@ -1,8 +1,30 @@
 import molest, os, datetime, _thread, sys, argparse
+
+
+parser = argparse.ArgumentParser(description="MolestLang")
+parser.add_argument("file",nargs="?",type=str,help="file to be executed",default="")
+args=parser.parse_args()
+if args.file:
+  try:
+    code=open(args.file,"r").read()
+    y=datetime.datetime.now()
+    result,error=molest.run(args.file,code)
+    x=datetime.datetime.now()
+    if error:
+        print(error.toString(),sep="\n")
+    else:
+        print(f"\nExecuted with zero errors in {(x-y).total_seconds()} seconds")
+  except KeyboardInterrupt:
+    sys.exit()
+  except Exception as e:
+    print("Could not find file, or fatal error...",e)
+  sys.exit()
+
+
 def begin(s,r):
   return s[:len(r)]==r
-print("MolestLang 0.0.0, type credits for more info")
-directory="C:/Swamipp/Programs/"
+print("MolestLang 0.0.1, type credits for more info")
+
 def notepad(f):
     os.system("notepad.exe "+directory+f)
     return
@@ -54,4 +76,4 @@ while 1:
     elif command=="help":
         print("Commands are file, run, programs, delete, and exit\nCheck the github page for syntax support")
     else:
-        print("Unkown command...\ntype help for help... ")
+        print("Unkown command...\ntype 'help' for help... ")
